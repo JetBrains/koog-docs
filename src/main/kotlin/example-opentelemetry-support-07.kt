365beb9fe835c0db918112d554c258c56fa3a3ce
Maria Tigina <tiginamaria@gmail.com>
2025-07-18 15:51:06 +0200

    Update opentelemetry-support.md

---
+++ src/main/kotlin/example-opentelemetry-support-07.kt
@@ -0,0 +1,27 @@
+// This file was automatically generated from opentelemetry-support.md by Knit tool. Do not edit.
+package ai.koog.agents.example.exampleOpentelemetrySupport07
+
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.features.opentelemetry.feature.OpenTelemetry
+import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
+
+const val apiKey = ""
+
+val agent = AIAgent(
+    executor = simpleOpenAIExecutor(apiKey),
+    llmModel = OpenAIModels.Chat.GPT4o,
+    systemPrompt = "You are a helpful assistant."
+) {
+
+install(OpenTelemetry) {
+   // Add OpenTelemetry gRPC exporter 
+   addSpanExporter(
+      OtlpGrpcSpanExporter.builder()
+          // Set the host and the port
+         .setEndpoint("http://localhost:4317")
+         .build()
+   )
+}
+}
